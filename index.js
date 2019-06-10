6061e92788c2bbbd6d5c1575e4a70a29c89a1cd2
Tyler Hunt <tyhunt04@gmail.com>
2019-06-10 22:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(obj, revenue) {
-  return obj.filter(function (driver) { return driver.revenue > revenue; })
-}
+  return obj.filter(function (driver) {
+    return driver.revenue > revenue;
+  })
+};
